13d04c2f60c48e3b00a6c78ec76158270783a9da
moetazgassoumi <moetazgassoumi3@gmail.com>
2025-02-25 02:41:09 +0100

    Satistiques gestion user

--- src/Repository/UtilisateurRepository.php
+++ src/Repository/UtilisateurRepository.php
@@ -8,6 +8,10 @@ use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Utilisateur>
+ *
+ * @method array findStatsByLieu()
+ * @method array findAgeStats()
+ * @method array findUsersGroupedByAgeRange()
  */
 class UtilisateurRepository extends ServiceEntityRepository
 {
@@ -16,28 +20,39 @@ class UtilisateurRepository extends ServiceEntityRepository
         parent::__construct($registry, Utilisateur::class);
     }
 
-    //    /**
-    //     * @return Utilisateur[] Returns an array of Utilisateur objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function findStatsByLieu(): array
+    {
+        return $this->createQueryBuilder('u')
+            ->select('u.lieu, COUNT(u.id) as total')
+            ->groupBy('u.lieu')
+            ->getQuery()
+            ->getResult();
+    }
 
-    //    public function findOneBySomeField($value): ?Utilisateur
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function findAgeStats(): array
+    {
+        return $this->createQueryBuilder('u')
+            ->select('AVG(u.age) as moyenne, MIN(u.age) as minimum, MAX(u.age) as maximum')
+            ->getQuery()
+            ->getSingleResult();
+    }
+
+    public function findUsersGroupedByAgeRange(): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = "SELECT 
+                    CASE
+                        WHEN age < 20 THEN '<20'
+                        WHEN age BETWEEN 20 AND 29 THEN '20-29'
+                        WHEN age BETWEEN 30 AND 39 THEN '30-39'
+                        ELSE '40+'
+                    END as ageRange,
+                    COUNT(*) as total
+                FROM utilisateur
+                GROUP BY ageRange
+                ORDER BY ageRange";
+        // Exécute directement la requête et récupère un Statement compatible avec DBAL 3
+        $stmt = $conn->executeQuery($sql);
+        return $stmt->fetchAllAssociative();
+    }
 }
