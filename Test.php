a89e169882a708a6e863c25c34f0d327f1aa32ac
mahdiselmi <131538046+mahdiselmi@users.noreply.github.com>
2025-02-28 10:06:22 +0100

    Add files via upload

---
+++ Test.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TestRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TestRepository::class)]
+class Test
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $idtest = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $matiere = null;
+
+    #[ORM\Column]
+    private ?float $heuredutest = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomduformateur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdtest(): ?int
+    {
+        return $this->idtest;
+    }
+
+    public function setIdtest(int $idtest): static
+    {
+        $this->idtest = $idtest;
+
+        return $this;
+    }
+
+    public function getMatiere(): ?string
+    {
+        return $this->matiere;
+    }
+
+    public function setMatiere(string $matiere): static
+    {
+        $this->matiere = $matiere;
+
+        return $this;
+    }
+
+    public function getHeuredutest(): ?float
+    {
+        return $this->heuredutest;
+    }
+
+    public function setHeuredutest(float $heuredutest): static
+    {
+        $this->heuredutest = $heuredutest;
+
+        return $this;
+    }
+
+    public function getNomduformateur(): ?string
+    {
+        return $this->nomduformateur;
+    }
+
+    public function setNomduformateur(string $nomduformateur): static
+    {
+        $this->nomduformateur = $nomduformateur;
+
+        return $this;
+    }
+}
